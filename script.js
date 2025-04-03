c441c878082037ce35cd1272ce3b9fe8db389311
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-04-03 15:37:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+	if(year%100 === 0){
+		if(year%400 === 0){
+			//leap yr
+			return 366;
+		}
+		else{
+			//not a leap yr
+			return 365;
+		}
+	}
+	else{
+		if(year%4 === 0){
+			//leap yr
+			return 366;
+		}
+		else{
+			return 365;
+		}
+	}
+	
+	
+}
+// dd
+let year = prompt("Enter year")
+// daysOfAYear(year)
+alert(daysOfAYear(year))
